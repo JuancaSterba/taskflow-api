6332c2cac5646c408f5b66e582a739970280c282
JuancaSterba <sjcexe@gmail.com>
2025-08-19 20:56:19 -0300

    feat: Setup Spring Boot project structure and initial health check endpoint

---
+++ src/test/java/com/juancasterba/taskflow_api/TaskflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juancasterba.taskflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
