b2eef916810acbc2e9eb3300128210a26154c69e
JuancaSterba <sjcexe@gmail.com>
2025-09-05 09:40:11 -0300

    Update AdminUserInitializer.java

--- src/main/java/com/juancasterba/taskflow_api/security/config/AdminUserInitializer.java
+++ src/main/java/com/juancasterba/taskflow_api/security/config/AdminUserInitializer.java
@@ -4,6 +4,7 @@ import com.juancasterba.taskflow_api.security.model.Role;
 import com.juancasterba.taskflow_api.security.model.User;
 import com.juancasterba.taskflow_api.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -15,15 +16,24 @@ public class AdminUserInitializer implements CommandLineRunner {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${spring.admin.username}")
+    private String ADMIN_USERNAME;
+
+    @Value("${spring.admin.password}")
+    private String ADMIN_PASSWORD;
+
+    @Value("${spring.admin.email}")
+    private String ADMIN_EMAIL;
+
     @Override
     public void run(String... args) throws Exception {
         // Verificamos si ya existe un usuario con el rol de ADMIN
         if (userRepository.findByRole(Role.ADMIN).isEmpty()) {
             User adminUser = new User();
-            adminUser.setUsername("admin");
-            adminUser.setEmail("admin@taskflow.com");
+            adminUser.setUsername(ADMIN_USERNAME);
+            adminUser.setEmail(ADMIN_EMAIL);
             // En un proyecto real, esta contraseña estaría en una variable de entorno
-            adminUser.setPassword(passwordEncoder.encode("adminPass"));
+            adminUser.setPassword(passwordEncoder.encode(ADMIN_PASSWORD));
             adminUser.setRole(Role.ADMIN);
 
             userRepository.save(adminUser);
