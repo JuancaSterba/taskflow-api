c03d87a4d6bc3669ff1151f6761a942ad4dc718e
JuancaSterba <sjcexe@gmail.com>
2025-09-09 20:48:48 -0300

    feat: added kafka-ui

--- src/main/java/com/juancasterba/taskflow_api/service/TaskServiceImpl.java
+++ src/main/java/com/juancasterba/taskflow_api/service/TaskServiceImpl.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
